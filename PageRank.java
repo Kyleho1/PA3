86d63931b633da1bc16e137498b8c83ff1c24309
ethang23 <ethang23@colostate.edu>
2025-11-05 22:31:17 -0700

    Implemented PageRank class methods

--- PageRank.java
+++ PageRank.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.jws.soap.SOAPBinding.Use;
 
 import org.apache.spark.api.java.JavaPairRDD;
@@ -5,23 +8,88 @@ import scala.Tuple2;
 
 public class PageRank {
 
-    public static JavaPairRDD<Integer, Double> idealPageRank(JavaPairRDD<Integer, java.util.List<Integer>> linksRDD, JavaPairRDD<Integer, Double> ranksRDD, int numIterations) {
-
-        //Implement ideal PageRank logic
-        //Parameters:
-        //linksRDD -> (fromPageId, [toPageIds])
-        //ranksRDD -> (pageId, rank)
-        //numIterations -> number of iterations
-        //my thoguhts but I could be wrong:
-        //Use linksRDD.join(ranksRDD), flatMapToPair to distribute ranks, and reduceByKey to sum contributions,Return the final ranksRDD after all iterations
-        return ranksRDD; // placeholder
+    public static JavaPairRDD<Integer, Double> idealPageRank(
+            JavaPairRDD<Integer, List<Integer>> linksRDD, 
+            JavaPairRDD<Integer, Double> ranksRDD, 
+            int numIterations) {
+        
+        for (int i = 0; i < numIterations; i++) {
+            JavaPairRDD<Integer, Double> contributions = linksRDD.join(ranksRDD)
+                .flatMapToPair(tuple -> {
+                    Integer pageId = tuple._1();
+                    List<Integer> outLinks = tuple._2()._1();
+                    Double rank = tuple._2()._2();
+                    
+                    List<Tuple2<Integer, Double>> results = new ArrayList<>();
+                    
+                    int numOutLinks = outLinks.size();
+                    
+                    if (numOutLinks > 0) {
+                        double contribution = rank / numOutLinks;
+                        for (Integer link : outLinks) {
+                            results.add(new Tuple2<>(link, contribution));
+                        }
+                    }
+                    
+                    return results.iterator();
+                });
+            
+            ranksRDD = linksRDD.leftOuterJoin(contributions.reduceByKey((a, b) -> a + b))
+                .mapValues(tuple -> {
+                    return tuple._2().isPresent() ? tuple._2().get() : 0.0;
+                });
+            
+            if ((i + 1) % 5 == 0) {
+                System.out.println("Ideal PageRank - Iteration " + (i + 1) + " completed");
+            }
+        }
+        
+        return ranksRDD;
     }
 
-    public static JavaPairRDD<Integer, Double> taxationPageRank( JavaPairRDD<Integer, java.util.List<Integer>> linksRDD, JavaPairRDD<Integer, Double> ranksRDD,int numIterations, double beta) {
-
-        // Thoughts: Add teleportation probability for pages with no out-links, return the final ranksRDD after all iterations
-        // Parameters:
-        // beta -> damping factor, usually 0.85
-        return ranksRDD; // placeholder
+     public static JavaPairRDD<Integer, Double> taxationPageRank(
+            JavaPairRDD<Integer, List<Integer>> linksRDD, 
+            JavaPairRDD<Integer, Double> ranksRDD,
+            int numIterations, 
+            double beta,
+            long numPages) {
+        
+        double taxationFactor = (1.0 - beta) / numPages;
+        
+        for (int i = 0; i < numIterations; i++) {
+            // Calculate contributions from following links
+            JavaPairRDD<Integer, Double> contributions = linksRDD.join(ranksRDD)
+                .flatMapToPair(tuple -> {
+                    Integer pageId = tuple._1();
+                    List<Integer> outLinks = tuple._2()._1();
+                    Double rank = tuple._2()._2();
+                    
+                    List<Tuple2<Integer, Double>> results = new ArrayList<>();
+                    
+                    int numOutLinks = outLinks.size();
+                    
+                    if (numOutLinks > 0) {
+                        double contribution = rank / numOutLinks;
+                        for (Integer link : outLinks) {
+                            results.add(new Tuple2<>(link, contribution));
+                        }
+                    }
+                    
+                    return results.iterator();
+                });
+            
+            // Apply taxation formula
+            ranksRDD = linksRDD.leftOuterJoin(contributions.reduceByKey((a, b) -> a + b))
+                .mapValues(tuple -> {
+                    double sum = tuple._2().isPresent() ? tuple._2().get() : 0.0;
+                    return beta * sum + taxationFactor;
+                });
+            
+            if ((i + 1) % 5 == 0) { // Print every 5 iterations
+                System.out.println("Taxation PageRank - Iteration " + (i + 1) + " completed");
+            }
+        }
+        
+        return ranksRDD;
     }
 }
